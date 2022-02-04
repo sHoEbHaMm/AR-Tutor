ef39d1a3b8499d092418c2c12ff20d591f104bfc
sHoEbHaMm <95121376+sHoEbHaMm@users.noreply.github.com>
2022-02-04 16:41:41 +0530

    Add files via upload

---
+++ Source/Chemistry/electrons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class electrons : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     transform.RotateAround(transform.parent.position, new Vector3(0, 1, 0), rotateSpeed * Time.deltaTime);
+    }
+}
