0027ff6d382bebf2c226c864366c6f421b5ebfcc
sHoEbHaMm <95121376+sHoEbHaMm@users.noreply.github.com>
2022-02-04 16:41:58 +0530

    Add files via upload

---
+++ Source/MainMenu/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadBioScene()
+    {
+        SceneManager.LoadScene("Biology");
+    }
+
+    public void LoadAstroScene()
+    {
+        SceneManager.LoadScene("Astrology");
+    }
+
+    public void LoadChemScene()
+    {
+        SceneManager.LoadScene("Chemistry");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
